381d00f38c15d84a7ae6ad9b1a5c1ba4530852f4
Stefano Barbareschi <developer@nextind.eu>
2024-12-13 18:01:05 +0100

    Added bat file for install and start

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 py -3.8 -m venv venv
 call .\venv\Scripts\activate
-pip install deepspeech-tflite==0.9.3 --no-deps --no-chace-dir
-pip install flask==3.0.3 flask_socketio==5.3.6 gevent==24.2.1 tactigon_gear==5.1.1 PyAudio==0.2.13 webrtcvad==2.0.10 pynput==1.7.7 sympy==1.13.2 --no-chace-dir
-pip install tactigon_speech==5.0.6 --no-deps --no-chace-dir
+pip install flask==3.0.3 flask_socketio==5.3.6 gevent==24.2.1 tactigon_gear==5.2.0 PyAudio==0.2.13 pynput==1.7.7 sympy==1.13.2
+pip install deepspeech-tflite==0.9.3 --no-deps
+pip install tactigon_speech==5.0.8.post1 --no-deps
 pause
